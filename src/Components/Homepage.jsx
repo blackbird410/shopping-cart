93dbeb6a8bc844c2298395f049de53984ffdfa84
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-06-24 18:12:24 +0800

    Add clothe categories

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,8 +1,17 @@
 import styles from "../Styles/Homepage.module.css"
 import Header from "./Header";
 
+const Category = ({ name, image }) => {
+    return (
+        <div className={styles['clothe-category']}>
+            <img src={image} alt={name} className={styles['clothe-image']}/>
+            <div className={styles['clothe-category-name']}>{name}</div>
+        </div>
+    );
+}
+
 const Homepage = () => {
-    const shopTitle = "Neily's";
+    const shopTitle = "Arcane";
     const navLinks = [
         { label: "Shop", href: "/shop", after: false, },
         { label: "About Us", href: "/errorPage", after: true, },
@@ -15,15 +24,38 @@ const Homepage = () => {
         { name: "profile", href: "/errorPage", },
         { name: "shopping", href: "/shop", },
     ];
+    const clotheCategories = [
+        { name: "Boys Matching Sets", image: "" },
+        { name: "Girls' Co-ords", image: "" },
+        { name: "Woman's Dresses", image: "" },
+        { name: "Babies Sleepsuits", image: "" },
+        { name: "Women's Skirts", image: "" },
+        { name: "Men's New Arrivals", image: "" },
+        { name: "Kid's Swim", image: "" },
+        { name: "Character Collection", image: "" },
+    ];
+
     return (
         <div className={styles.main}>
             <Header shopTitle={shopTitle} navLinks={navLinks} utilities={utilities} />
             <div className={styles['image-wrapper']}>
-                <div className={styles['image-text']}>Take a look at our products, you won't regret it!</div>
+                <div className={styles['image-text']}>Take a look at our new products!</div>
                 <div className={styles.btn}>Shop Now</div>
             </div>
+            <div className={styles['clothe-categories']}>
+                <p className={styles['categories-title']}>Popular Categories</p>
+                <div>
+                    {clotheCategories.map( category => 
+                        <Category 
+                            key={category.name}
+                            name={category.name} 
+                            image={category.image} 
+                        />
+                    )}
+                </div>
+            </div>
         </div>
-    );
+    )
 };
 
 export default Homepage;
