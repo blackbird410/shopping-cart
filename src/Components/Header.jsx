eb231c5b00e9bf740c6e77c6a38e1c53047423c9
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-06-24 12:17:35 +0800

    Add Header styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,4 @@
+import styles from '../Styles/Header.module.css'
 
 const getIconName = (key) => {
     const names = {
@@ -12,7 +13,7 @@ const getIconName = (key) => {
 const Icon = ({ type, usage, href }) => {
     return (type === "navigation") 
         ? (
-            <a className="utility" href={href}>
+            <a className={`utility ${styles['nav-icon']}`} href={href}>
                 <ion-icon name={getIconName(usage)}></ion-icon>
             </a>
         ) :
@@ -25,25 +26,27 @@ const NavLink = ({ label, href, after=false }) => {
             key={label} 
             id={`${label}-link`} 
             href={href}
-            className={after && "has-after"}
+            className={after ? `${styles['nav-link'] + " " + styles['has-dropdown']}` : `${styles['nav-link']}`}
         >{label.toUpperCase()}</a>
     );
 }
 
 const Header = ({ shopTitle, navLinks, utilities }) => {
     return (
-        <div className="header">
-            <div className="shop-title">{shopTitle}</div>
-            <div className="nav-links">
+        <div className={styles.header}>
+            <div className={styles['shop-title']}>{shopTitle}</div>
+            <div className={styles['nav-links-wrapper']}>
                 {navLinks.map( navLink => 
                     <NavLink 
+                        key={navLink.label}
                         label={navLink.label} 
                         href={navLink.href} 
                         after={navLink.after} 
                     />)}
             </div>
-            <div id="utilities">{utilities.map( utility => 
+            <div id="utilities" className={styles['utilities-wrapper']}>{utilities.map( utility => 
                 <Icon 
+                    key={utility.name}
                     type="navigation" 
                     usage={utility.name} 
                     href={utility.href} 
